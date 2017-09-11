4d2dbddfa9433f777452dd547bdf00d79f22b4c9
feng416761494 <416761494@qq.com>
2017-09-11 10:16:30 +0800

    aaa

--- app/src/main/java/com/usamsl/global/index/fragment/IndexFragment.java
+++ app/src/main/java/com/usamsl/global/index/fragment/IndexFragment.java
@@ -68,7 +68,7 @@ import okhttp3.Response;
  * 文件名：IndexFragment
  * 描述：主界面
  * 时间：2016/12/14
- * 作者：冯井起test!!!!
+ * 作者：888888
  */
 public class IndexFragment extends Fragment implements AMapLocationListener,View.OnClickListener {
 
